2955c7e9a8779fe1ec92846b4e6c06db5b314a64
MichaelYe48 <mhye@ucsd.edu>
2023-05-21 14:59:23 -0700

    first push for explore

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -45,15 +45,29 @@ function initializeServiceWorker() {
   // We first must register our ServiceWorker here before any of the code in
   // sw.js is executed.
   // B1. TODO - Check if 'serviceWorker' is supported in the current browser
+  if ("serviceWorker" in navigator) {
   // B2. TODO - Listen for the 'load' event on the window object.
+    window.addEventListener("load", (event) => {
   // Steps B3-B6 will be *inside* the event listener's function created in B2
   // B3. TODO - Register './sw.js' as a service worker (The MDN article
   //            "Using Service Workers" will help you here)
+      try {
+        const registration = navigator.serviceWorker.register("sw.js", {scope: "."});
   // B4. TODO - Once the service worker has been successfully registered, console
   //            log that it was successful.
+        if (registration.installing || registration.waiting || registration.active) {
+          console.log("Registration Successful");
+        }
+      }
   // B5. TODO - In the event that the service worker registration fails, console
-  //            log that it has failed.
+  //            log that it has failed. 
+      catch (error) {
+        console.error(`Registration failed with ${error}`);
+      }
+    });
+  }
   // STEPS B6 ONWARDS WILL BE IN /sw.js
+
 }
 
 /**
@@ -69,9 +83,7 @@ async function getRecipes() {
   // A1. TODO - Check local storage to see if there are any recipes.
   //            If there are recipes, return them.
   if (localStorage.getItem("recipes")) {
-    if (localStorage.getItem("recipes").length > 0) {
-      return JSON.parse(localStorage.getItem("recipes"));
-    }
+    return JSON.parse(localStorage.getItem("recipes"));
   }
   /**************************/
   // The rest of this method will be concerned with requesting the recipes
